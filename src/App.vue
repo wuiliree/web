44dbd8bd5a552bd42eb057f4f78ba497e2334c27
wuiliree <wuiliree@163.com>
2023-06-14 22:36:02 +0800

    计算属性

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -82,4 +82,89 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
+
+<template>
+  <div>
+    <div>
+      <input placeholder="搜索" type="text" v-model="keyWord">
+    </div>
+    <div style="margin-top: 20px;">
+      <table border cellpadding="0" cellspacing="0" width="600">
+        <thead>
+          <tr>
+            <th>物品名称</th>
+            <th>物品单价</th>
+            <th>物品数量</th>
+            <th>物品总价</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in searchData">
+            <td align="center">{{ item.name }}</td>
+            <td align="center">{{ item.price }}</td>
+            <td align="center">
+              <button @click="item.num > 1 ? item.num-- : null">-</button>
+              {{ item.num }}
+              <button @click="item.num < 99 ? item.num++ : null">+</button>
+            </td>
+            <td align="center">{{ item.num * item.price }}</td>
+            <td align="center"><button @click="del(index)">删除</button></td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="5" align="right">
+              总价：{{ total }}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { computed, reactive, ref } from 'vue';
+
+let keyWord = ref<string>("")
+interface Data {
+  name: string;
+  price: number;
+  num: number;
+}
+let data = reactive<Data[]>([
+  {
+    name: "小满的绿帽子",
+    price: 500,
+    num: 1
+  },
+  {
+    name: "小满的衣服",
+    price: 1000,
+    num: 1
+  },
+  {
+    name: "小满的裤子",
+    price: 500,
+    num: 1
+  },
+])
+const searchData = computed(() => {
+  return data.filter((item:Data) => {
+    return item.name.includes(keyWord.value)
+  })
+})
+const total = computed(() => {
+  return data.reduce((prev: number, next: Data) => {
+    return prev + next.num * next.price
+  }, 0)
+})
+const del = (index: number) => {
+  data.splice(index, 1)
+}
+
+</script>
+
+<style scoped lang='less'></style>
